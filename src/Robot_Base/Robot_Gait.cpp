440e6359ccfff0e29f65d65b598b872404334de4
Qiao Sun <qiaosun1234@gmail.com>
2016-08-24 18:42:21 +0800

    Add require zeroing option for recover gait

--- src/Robot_Base/Robot_Gait.cpp
+++ src/Robot_Base/Robot_Gait.cpp
@@ -137,6 +137,10 @@ namespace Robots
 			{
 				param.margin_offset = std::stod(i.second);
 			}
+            else if (i.first == "require_zero")
+            {
+                param.is_zeroing_required = true;
+            }
 			else
 			{
 				throw std::runtime_error("unknown param in parseRecover func");
@@ -193,6 +197,11 @@ namespace Robots
 
 					robot.pLegs[i]->SetPee(pEE);
 				}
+
+                if (param.is_zeroing_required && param.count == param.recover_count)
+                {
+                    param.force_data->at(i).isZeroingRequested = true;
+                }
 			}
 		}
 
